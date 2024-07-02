9de768d81462b49cc1b2f91c7acdba09a1687b87
Pantoja25 <165227131+Pantoja25@users.noreply.github.com>
2024-07-02 20:10:06 -0300

    Add Tela PrincipalAdicionar arquivos de projeto.

---
+++ TelaPrincipal.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace SysVenda_OBSEQUION {
+	partial class TelaPrincipal {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			components = new System.ComponentModel.Container();
+			contextMenuStrip1 = new ContextMenuStrip(components);
+			contextMenuStrip2 = new ContextMenuStrip(components);
+			menuStrip1 = new MenuStrip();
+			cADRASTOToolStripMenuItem = new ToolStripMenuItem();
+			cLIENTEToolStripMenuItem = new ToolStripMenuItem();
+			pRODUTOToolStripMenuItem = new ToolStripMenuItem();
+			vENDAToolStripMenuItem = new ToolStripMenuItem();
+			iTENSDEVENDASToolStripMenuItem = new ToolStripMenuItem();
+			uSUARIOToolStripMenuItem = new ToolStripMenuItem();
+			vENDASToolStripMenuItem = new ToolStripMenuItem();
+			contextMenuStrip3 = new ContextMenuStrip(components);
+			menuStrip1.SuspendLayout();
+			SuspendLayout();
+			// 
+			// contextMenuStrip1
+			// 
+			contextMenuStrip1.Name = "contextMenuStrip1";
+			contextMenuStrip1.Size = new Size(61, 4);
+			// 
+			// contextMenuStrip2
+			// 
+			contextMenuStrip2.Name = "contextMenuStrip2";
+			contextMenuStrip2.Size = new Size(61, 4);
+			// 
+			// menuStrip1
+			// 
+			menuStrip1.Items.AddRange(new ToolStripItem[] { cADRASTOToolStripMenuItem, vENDASToolStripMenuItem });
+			menuStrip1.Location = new Point(0, 0);
+			menuStrip1.Name = "menuStrip1";
+			menuStrip1.Size = new Size(800, 24);
+			menuStrip1.TabIndex = 2;
+			menuStrip1.Text = "menuStrip1";
+			// 
+			// cADRASTOToolStripMenuItem
+			// 
+			cADRASTOToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { cLIENTEToolStripMenuItem, pRODUTOToolStripMenuItem, vENDAToolStripMenuItem, iTENSDEVENDASToolStripMenuItem, uSUARIOToolStripMenuItem });
+			cADRASTOToolStripMenuItem.Name = "cADRASTOToolStripMenuItem";
+			cADRASTOToolStripMenuItem.Size = new Size(79, 20);
+			cADRASTOToolStripMenuItem.Text = "CADASTRO";
+			cADRASTOToolStripMenuItem.Click += cADRASTOToolStripMenuItem_Click;
+			// 
+			// cLIENTEToolStripMenuItem
+			// 
+			cLIENTEToolStripMenuItem.Name = "cLIENTEToolStripMenuItem";
+			cLIENTEToolStripMenuItem.Size = new Size(180, 22);
+			cLIENTEToolStripMenuItem.Text = "CLIENTE";
+			cLIENTEToolStripMenuItem.Click += cLIENTEToolStripMenuItem_Click;
+			// 
+			// pRODUTOToolStripMenuItem
+			// 
+			pRODUTOToolStripMenuItem.Name = "pRODUTOToolStripMenuItem";
+			pRODUTOToolStripMenuItem.Size = new Size(180, 22);
+			pRODUTOToolStripMenuItem.Text = "PRODUTO";
+			// 
+			// vENDAToolStripMenuItem
+			// 
+			vENDAToolStripMenuItem.Name = "vENDAToolStripMenuItem";
+			vENDAToolStripMenuItem.Size = new Size(180, 22);
+			vENDAToolStripMenuItem.Text = "VENDA";
+			// 
+			// iTENSDEVENDASToolStripMenuItem
+			// 
+			iTENSDEVENDASToolStripMenuItem.Name = "iTENSDEVENDASToolStripMenuItem";
+			iTENSDEVENDASToolStripMenuItem.Size = new Size(180, 22);
+			iTENSDEVENDASToolStripMenuItem.Text = "ITENS DE VENDAS";
+			// 
+			// uSUARIOToolStripMenuItem
+			// 
+			uSUARIOToolStripMenuItem.Name = "uSUARIOToolStripMenuItem";
+			uSUARIOToolStripMenuItem.Size = new Size(180, 22);
+			uSUARIOToolStripMenuItem.Text = "USUARIO";
+			// 
+			// vENDASToolStripMenuItem
+			// 
+			vENDASToolStripMenuItem.Name = "vENDASToolStripMenuItem";
+			vENDASToolStripMenuItem.Size = new Size(63, 20);
+			vENDASToolStripMenuItem.Text = "VENDAS";
+			// 
+			// contextMenuStrip3
+			// 
+			contextMenuStrip3.Name = "contextMenuStrip3";
+			contextMenuStrip3.Size = new Size(61, 4);
+			// 
+			// TelaPrincipal
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(menuStrip1);
+			Name = "TelaPrincipal";
+			Text = "TelaPrincipal";
+			menuStrip1.ResumeLayout(false);
+			menuStrip1.PerformLayout();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private ContextMenuStrip contextMenuStrip1;
+		private ContextMenuStrip contextMenuStrip2;
+		private MenuStrip menuStrip1;
+		private ToolStripMenuItem cADRASTOToolStripMenuItem;
+		private ToolStripMenuItem cLIENTEToolStripMenuItem;
+		private ToolStripMenuItem pRODUTOToolStripMenuItem;
+		private ToolStripMenuItem vENDAToolStripMenuItem;
+		private ToolStripMenuItem iTENSDEVENDASToolStripMenuItem;
+		private ToolStripMenuItem vENDASToolStripMenuItem;
+		private ContextMenuStrip contextMenuStrip3;
+		private ToolStripMenuItem uSUARIOToolStripMenuItem;
+	}
+}
