4b51959f95ae8992ef74bb087f95bc7c31e79894
Pantoja25 <165227131+Pantoja25@users.noreply.github.com>
2024-07-02 21:05:44 -0300

    Add entidades

---
+++ entidades/ItemVenda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysVenda_OBSEQUION.entidades {
+	internal class ItemVenda {
+			public int ItemVendaId { get; set; }
+			public string Titulo { get; set; }
+			public string Descricao { get; set; } 
+			public decimal Preco { get; set; } 
+			public string Categoria { get; set; } 
+			public string ImagemURL { get; set; } 
+			public int QuantidadeEmEstoque { get; set; } 
+			public bool Disponivel { get; set; } 
+		}
+
+	}
+}
---
+++ entidades/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysVenda_OBSEQUION.entidades {
+	internal class Usuario {
+		public int UsuarioId { get; set; }
+		public string UsuarioName { get; set; }
+		public string Login { get; set; }
+		public string Senha { get; set; }
+		public string Tipo { get; set; }
+		public string Email { get; set; } 
+		public DateTime DataCriacao { get; set; } 
+		public bool Ativo { get; set; } 
+		public string Endereco { get; set; } 
+		public string Telefone { get; set; } 
+	}
+
+}
+
