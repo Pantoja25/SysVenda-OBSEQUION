d34063f9807ed82dabee609c01b2582270087afa
LucianoMatosDeAlmeida <luciano51607786@edu.pa.senac.br>
2024-07-03 20:32:48 -0300

    Add Tela Cliente e seus Elementos

--- Form1.cs
+++
@@ -1,7 +0,0 @@
-namespace SysVenda_OBSEQUION {
-	public partial class Form1 : Form {
-		public Form1() {
-			InitializeComponent();
-		}
-	}
-}
---
+++ Form_Cliente.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace SysVenda_OBSEQUION {
+	partial class Form_Cliente {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			dataGridView1 = new DataGridView();
+			button1 = new Button();
+			button2 = new Button();
+			button3 = new Button();
+			textBox1 = new TextBox();
+			textBox2 = new TextBox();
+			textBox3 = new TextBox();
+			textBox4 = new TextBox();
+			textBox5 = new TextBox();
+			textBox6 = new TextBox();
+			textBox7 = new TextBox();
+			textBox8 = new TextBox();
+			label1 = new Label();
+			label2 = new Label();
+			label3 = new Label();
+			label4 = new Label();
+			label5 = new Label();
+			label6 = new Label();
+			label7 = new Label();
+			button4 = new Button();
+			((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+			SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dataGridView1.Location = new Point(33, 234);
+			dataGridView1.Name = "dataGridView1";
+			dataGridView1.Size = new Size(729, 136);
+			dataGridView1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			button1.Location = new Point(687, 398);
+			button1.Name = "button1";
+			button1.Size = new Size(75, 40);
+			button1.TabIndex = 1;
+			button1.Text = "button1";
+			button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			button2.Location = new Point(592, 398);
+			button2.Name = "button2";
+			button2.Size = new Size(75, 40);
+			button2.TabIndex = 2;
+			button2.Text = "button2";
+			button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			button3.Location = new Point(54, 398);
+			button3.Name = "button3";
+			button3.Size = new Size(75, 40);
+			button3.TabIndex = 3;
+			button3.Text = "button3";
+			button3.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(67, 80);
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(136, 23);
+			textBox1.TabIndex = 5;
+			// 
+			// textBox2
+			// 
+			textBox2.Location = new Point(105, 128);
+			textBox2.Name = "textBox2";
+			textBox2.Size = new Size(410, 23);
+			textBox2.TabIndex = 6;
+			// 
+			// textBox3
+			// 
+			textBox3.Location = new Point(601, 176);
+			textBox3.Name = "textBox3";
+			textBox3.Size = new Size(161, 23);
+			textBox3.TabIndex = 7;
+			// 
+			// textBox4
+			// 
+			textBox4.Location = new Point(89, 176);
+			textBox4.Name = "textBox4";
+			textBox4.Size = new Size(426, 23);
+			textBox4.TabIndex = 8;
+			// 
+			// textBox5
+			// 
+			textBox5.Location = new Point(592, 80);
+			textBox5.Name = "textBox5";
+			textBox5.Size = new Size(170, 23);
+			textBox5.TabIndex = 9;
+			// 
+			// textBox6
+			// 
+			textBox6.Location = new Point(306, 80);
+			textBox6.Name = "textBox6";
+			textBox6.Size = new Size(100, 23);
+			textBox6.TabIndex = 10;
+			// 
+			// textBox7
+			// 
+			textBox7.Location = new Point(644, 128);
+			textBox7.Name = "textBox7";
+			textBox7.Size = new Size(118, 23);
+			textBox7.TabIndex = 11;
+			// 
+			// textBox8
+			// 
+			textBox8.Location = new Point(89, 33);
+			textBox8.Name = "textBox8";
+			textBox8.Size = new Size(673, 23);
+			textBox8.TabIndex = 12;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(33, 36);
+			label1.Name = "label1";
+			label1.Size = new Size(42, 15);
+			label1.TabIndex = 13;
+			label1.Text = "NOME";
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(33, 83);
+			label2.Name = "label2";
+			label2.Size = new Size(28, 15);
+			label2.TabIndex = 14;
+			label2.Text = "CPF";
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Location = new Point(278, 83);
+			label3.Name = "label3";
+			label3.Size = new Size(22, 15);
+			label3.TabIndex = 15;
+			label3.Text = "RG";
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			label4.Location = new Point(495, 83);
+			label4.Name = "label4";
+			label4.Size = new Size(81, 15);
+			label4.TabIndex = 16;
+			label4.Text = "NASCIMENTO";
+			// 
+			// label5
+			// 
+			label5.AutoSize = true;
+			label5.Location = new Point(33, 131);
+			label5.Name = "label5";
+			label5.Size = new Size(66, 15);
+			label5.TabIndex = 17;
+			label5.Text = "ENDERECO";
+			// 
+			// label6
+			// 
+			label6.AutoSize = true;
+			label6.Location = new Point(558, 179);
+			label6.Name = "label6";
+			label6.Size = new Size(37, 15);
+			label6.TabIndex = 18;
+			label6.Text = "FONE";
+			// 
+			// label7
+			// 
+			label7.AutoSize = true;
+			label7.Location = new Point(34, 179);
+			label7.Name = "label7";
+			label7.Size = new Size(41, 15);
+			label7.TabIndex = 19;
+			label7.Text = "EMAIL";
+			// 
+			// button4
+			// 
+			button4.Location = new Point(558, 128);
+			button4.Name = "button4";
+			button4.Size = new Size(75, 23);
+			button4.TabIndex = 20;
+			button4.Text = "BUSCAR";
+			button4.UseVisualStyleBackColor = true;
+			// 
+			// Form_Cliente
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(button4);
+			Controls.Add(label7);
+			Controls.Add(label6);
+			Controls.Add(label5);
+			Controls.Add(label4);
+			Controls.Add(label3);
+			Controls.Add(label2);
+			Controls.Add(label1);
+			Controls.Add(textBox8);
+			Controls.Add(textBox7);
+			Controls.Add(textBox6);
+			Controls.Add(textBox5);
+			Controls.Add(textBox4);
+			Controls.Add(textBox3);
+			Controls.Add(textBox2);
+			Controls.Add(textBox1);
+			Controls.Add(button3);
+			Controls.Add(button2);
+			Controls.Add(button1);
+			Controls.Add(dataGridView1);
+			Name = "Form_Cliente";
+			Text = "Form_Cliente";
+			((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private DataGridView dataGridView1;
+		private Button button1;
+		private Button button2;
+		private Button button3;
+		private TextBox textBox1;
+		private TextBox textBox2;
+		private TextBox textBox3;
+		private TextBox textBox4;
+		private TextBox textBox5;
+		private TextBox textBox6;
+		private TextBox textBox7;
+		private TextBox textBox8;
+		private Label label1;
+		private Label label2;
+		private Label label3;
+		private Label label4;
+		private Label label5;
+		private Label label6;
+		private Label label7;
+		private Button button4;
+	}
+}
