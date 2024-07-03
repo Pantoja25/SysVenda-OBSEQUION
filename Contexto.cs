4e66aaccf381bfea16e65014075d5586ba127a1d
CaduMlkDoido <carlos51633606@edu.pa.senac.br>
2024-07-02 21:32:30 -0300

    Criado classe Contexto e seus atributos

---
+++ Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SysVendas2.Entidades;
+
+namespace SysVendas2
+{
+    internal class Contexto : BdContext
+    {
+        public BdSet<Cliente> Clientes { get; set; }
+        public BdSet<Produto> Produtos { get; set; }
+        public BdSet<Venda> Vendas { get; set; }
+        public BdSet<ItemVenda> ItensVenda { get; set; }
+        public BdSet<Usuario> Usuarios { get; set; }
+
+    }
+}
