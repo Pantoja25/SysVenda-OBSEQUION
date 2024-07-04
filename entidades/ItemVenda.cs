76159ad85e31e0fe12faf9bacb21c25eb5cee391
Pantoja25 <165227131+Pantoja25@users.noreply.github.com>
2024-07-04 20:15:17 -0300

    add correção

--- entidades/ItemVenda.cs
+++ entidades/ItemVenda.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SysVenda_OBSEQUION.entidades {
 	internal class ItemVenda {
-			public int ItemVendaId { get; set; }
-			public string Titulo { get; set; }
-			public string Descricao { get; set; } 
-			public decimal Preco { get; set; } 
-			public string Categoria { get; set; } 
-			public string ImagemURL { get; set; } 
-			public int QuantidadeEmEstoque { get; set; } 
-			public bool Disponivel { get; set; } 
-	}
+		[Kay]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int ItemVendaId { get; set; }
+		public int VendaId { get; set; }
+		public int ProdutoId { get; set; }
+		public string Titulo { get; set; }
+		public string Descricao { get; set; }
+		public decimal Preco { get; set; }
+		public bool Disponivel { get; set; }
+		public decimal Quantidade { get; set; }
+		public decimal ValorUnitario { get; set; }
+		public decimal Subtotal {
+			get { return ValorUnitario * Quantidade; }
 
+		}
+
+		public Venda Venda { get; set; }
+		public Produtos Produtos { get; set; }
+	}
 }
